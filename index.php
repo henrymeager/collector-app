a90af18988b59b5b25b7ae6231bd6c576811b9d6
Henry Meager <henry.meager@gmail.com>
2023-11-20 14:26:53 +0000

    fixing resolutions + added tests file

--- index.php
+++ index.php
@@ -1,32 +1,20 @@
 <?php
 
-/**
- * This file represents our home page, it's where use all of our objects to
- * display the page
- */
+require_once 'src/VideogameModel.php';
+require_once 'src/VideogameViewHelper.php';
 
-
-// Require in any of the classes we need to use
-require_once 'src/VideogamesModel.php';
-require_once 'src/VideogamesViewHelper.php';
-
-// Created a DB connection
 $db = new PDO('mysql:host=db; dbname=project', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-// We need to get some products to display them, so we use the ProductModel
-// which already has all the code we need
-$videogamesModel = new VideogamesModel($db); // We pass $db in, a Model needs a database connection
-// in order to do it's job
+$videogamesModel = new VideogamesModel($db);
 
-// And now we can use our model to run any DB queries we need - getting all products
-// in this case
 $videogames = $videogamesModel->getAllVideogames();
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -34,21 +22,14 @@ $videogames = $videogamesModel->getAllVideogames();
     <link rel="stylesheet" type="text/css" href="mediaqueries.css">
     <title>Videogame Collector's Site</title>
 </head>
+
 <body>
     <div class="grid-container">
         <?php
-        foreach ($videogames as $videogame) {
-            echo '<div class="grid-item">';
-            echo '<h3>' . $videogame->name . '</h3>';
-            echo '<p>Release Year: ' . $videogame->release_year . '</p>';
-            echo '<p>Platform: ' . $videogame->platform_name . '</p>';
-            echo '</div>';
-        }
+        $allVideogames = $videogamesModel->getAllVideogames();
+        echo VideogamesViewHelper::displayAllVideogames($allVideogames);
         ?>
     </div>
 </body>
+
 </html>
-
-
-
-
