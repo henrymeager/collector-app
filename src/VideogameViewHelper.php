5552072053c40097ebc4ffdf14067368b32f2ffc
Henry Meager <henry.meager@gmail.com>
2023-11-21 21:48:06 +0000

    added edit button and started working on the edit form

--- src/VideogameViewHelper.php
+++ src/VideogameViewHelper.php
@@ -20,20 +20,21 @@ class VideogamesViewHelper
         $output = '';
 
         foreach ($videogames as $videogame) {
-                $output .= '<div class="grid-item">';
-                $output .= '<h3>' . $videogame->name . '</h3>';
-                $output .= '<p>Release Year: ' . $videogame->release_year . '</p>';
-                $output .= '<p>Platform: ' . $videogame->platform_name . '</p>';
+            $output .= '<div class="grid-item">';
+            $output .= '<h3>' . $videogame->name . '</h3>';
+            $output .= '<p>Release Year: ' . $videogame->release_year . '</p>';
+            $output .= '<p>Platform: ' . $videogame->platform_name . '</p>';
 
-                $output .= '<form method="post">';
-                $output .= '<input type="hidden" name="videogame_id" value="' . $videogame->id . '">';
-                $output .= '<button type="submit" name="delete_button">Delete</button>';
-                $output .= '</form>';
+            $output .= '<form method="post" action="EditVideogame.php">';
+            $output .= '<input type="hidden" name="videogame_id" value="' . $videogame->id . '">';
+            $output .= '<button type="submit" name="delete_button">Delete</button>';
+            $output .= '<button type="submit" name="edit_button">Edit</button>';
+            $output .= '</form>';
 
-                $output .= '</div>';
+
+            $output .= '</div>';
         }
 
         return $output;
     }
 }
-?>
