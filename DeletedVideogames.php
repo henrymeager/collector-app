15c55d0597158ec66e49f7b850e1f3de34af148c
Henry Meager <henry.meager@gmail.com>
2023-11-22 17:52:49 +0000

    adding deleted games file

---
+++ DeletedVideogames.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once 'src/VideogameModel.php';
+require_once 'src/VideogameViewHelper.php';
+
+$db = new PDO('mysql:host=db; dbname=project', 'root', 'password');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$videogamesModel = new VideogamesModel($db);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['restore_button'])) {
+    if (isset($_POST['id'])) {
+        $id = (int)$_POST['id'];
+
+        $stmt = $db->prepare("UPDATE videogames SET is_deleted = 0 WHERE id = ?");
+        $stmt->execute([$id]);
+    }
+}
+
+$allVideogames = $videogamesModel->getAllDeletedVideogames();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="mediaqueries.css">
+    <title>CollectorSite</title>
+</head>
+
+<body>
+
+    <div class="nav-bar">
+        <nav>
+        <a href="index.php">Home</a>
+            <a href="AddVideogame.php">AddGame</a>
+            <a href="DeletedVideogames.php">RestoreGame</a>
+        </nav>
+    </div>
+
+    <div class="grid-container">
+        <?php
+        echo VideogamesViewHelper::displayAllDeletedVideogames($allVideogames);
+        ?>
+    </div>
+</body>
+
+</html>
+
